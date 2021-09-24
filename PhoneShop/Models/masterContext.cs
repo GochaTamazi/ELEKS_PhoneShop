88deefa4c7f7f546189bab2c6a5c9330c8fbd491
Vitalii Platonov <zipocat@gmail.com>
2021-09-24 17:20:05 +0300

    EntityFrameworkCore

---
+++ PhoneShop/Models/masterContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PhoneShop.Models
+{
+    public partial class masterContext : DbContext
+    {
+        public masterContext()
+        {
+        }
+
+        public masterContext(DbContextOptions<masterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Test> Tests { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost; user=SA; password=456789Zxc");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<Test>(entity =>
+            {
+                entity.HasKey(e => e.Id)
+                    .HasName("test_pk")
+                    .IsClustered(false);
+
+                entity.ToTable("test", "_public");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
