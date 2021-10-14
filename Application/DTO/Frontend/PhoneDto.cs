a7db5840467a4836aa9d825b39d54e9dc94ab87d
Vitalii Platonov <zipocat@gmail.com>
2021-10-14 23:11:20 +0300

    fix

---
+++ Application/DTO/Frontend/PhoneDto.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Application.DTO.PhoneSpecificationsAPI.PhoneSpecifications;
+
+namespace Application.DTO.Frontend
+{
+    public class PhoneDto
+    {
+        public int Id { get; set; }
+        public string BrandSlug { get; set; }
+        public string PhoneSlug { get; set; }
+        public string PhoneName { get; set; }
+        public string Dimension { get; set; }
+        public string Os { get; set; }
+        public string Storage { get; set; }
+        public string Thumbnail { get; set; }
+        public string ReleaseDate { get; set; }
+        public List<string> Images { get; set; } = new List<string>();
+        public ICollection<SpecificationDto> Specifications { get; set; } = new List<SpecificationDto>();
+        public int? Price { get; set; }
+        public int? Stock { get; set; }
+        public bool? Hided { get; set; }
+    }
+}
