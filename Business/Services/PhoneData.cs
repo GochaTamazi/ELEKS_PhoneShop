c6b9ca577271bd92affeabb8c40c870dbde12667
Vitalii Platonov <zipocat@gmail.com>
2021-11-21 23:59:36 +0200

    Template for Phones export xlsx

--- Business/Services/PhoneData.cs
+++ Business/Services/PhoneData.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,54 +12,36 @@ namespace Application.Services
     {
         public async Task<byte[]> ExportToXlsxAsync(List<Phone> phones, CancellationToken token)
         {
-            var dt = new DataTable();
-            dt.TableName = "PhonesData";
+            const string filePath = "./Templates/xlsx/Phones.xlsx";
+            using var workBook = new XLWorkbook(filePath);
+            var workSheet = workBook.Worksheet(1);
 
-            //Add Columns
-            dt.Columns.Add("Id", typeof(int));
-            dt.Columns.Add("BrandSlug", typeof(string));
-            dt.Columns.Add("PhoneSlug", typeof(string));
-            dt.Columns.Add("PhoneName", typeof(string));
-            dt.Columns.Add("Dimension", typeof(string));
-            dt.Columns.Add("Os", typeof(string));
-            dt.Columns.Add("Storage", typeof(string));
-            dt.Columns.Add("ReleaseDate", typeof(string));
-            dt.Columns.Add("Price", typeof(int));
-            dt.Columns.Add("Stock", typeof(int));
-            dt.Columns.Add("Hided", typeof(string));
-            dt.Columns.Add("Thumbnail", typeof(string));
-            dt.Columns.Add("Images", typeof(string));
-            dt.Columns.Add("Specifications", typeof(string));
-
+            var i = 2;
             foreach (var phone in phones)
             {
-                dt.Rows.Add(phone.Id,
-                    phone.BrandSlug,
-                    phone.PhoneSlug,
-                    phone.PhoneName,
-                    phone.Dimension,
-                    phone.Os,
-                    phone.Storage,
-                    phone.ReleaseDate,
-                    phone.Price,
-                    phone.Stock,
-                    phone.Hided,
-                    phone.Thumbnail,
-                    phone.Images,
-                    phone.Specifications);
+                workSheet.Cell("A" + i).Value = phone.Id;
+                workSheet.Cell("B" + i).Value = phone.BrandSlug;
+                workSheet.Cell("C" + i).Value = phone.PhoneSlug;
+                workSheet.Cell("D" + i).Value = phone.PhoneName;
+                workSheet.Cell("E" + i).Value = phone.Dimension;
+                workSheet.Cell("F" + i).Value = phone.Os;
+                workSheet.Cell("G" + i).Value = phone.Storage;
+                workSheet.Cell("H" + i).Value = phone.ReleaseDate;
+                workSheet.Cell("I" + i).Value = phone.Price;
+                workSheet.Cell("J" + i).Value = phone.Stock;
+                workSheet.Cell("K" + i).Value = phone.Hided;
+                workSheet.Cell("L" + i).Value = phone.Thumbnail;
+                workSheet.Cell("M" + i).Value = phone.Images;
+                workSheet.Cell("N" + i).Value = phone.Specifications;
+                i++;
             }
 
-            dt.AcceptChanges();
-
-            using var workbook = new XLWorkbook();
-            var sheet = workbook.Worksheets.Add(dt);
-            sheet.RowHeight = 20;
-            sheet.ColumnWidth = 23;
-
             await using var ms = new MemoryStream();
-            workbook.SaveAs(ms);
+            workBook.SaveAs(ms);
             ms.Position = 0;
-            return ms.ToArray();
+            var array = ms.ToArray();
+            ms.Close();
+            return array;
         }
     }
 }
